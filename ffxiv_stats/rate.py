54a0e2acb020a89d8a31dc169044d86234020b3e
acerola <ffxivacerola@gmail.com>
2024-01-24 21:45:36 -0600

    add method to compute damage buff on guaranteed hit types when the hit type is guaranteed.

--- ffxiv_stats/rate.py
+++ ffxiv_stats/rate.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from .modifiers import level_mod
 
 
@@ -63,7 +64,7 @@ class Rate:
         ch_mod: Percentage to increase the base critical hit rate by if buffs are present.
                 E.g., ch_mod=0.1 would add 0.1 to the base critical hit rate
         dh_mod: Percentage to increase the base direct hit rate by if buffs are present.
-        guaranteed_hit_type: get probability for a guaranteed hit type. 
+        guaranteed_hit_type: get probability for a guaranteed hit type.
                              Parameters ch_mod and dh_mod have no effect if this is non-zero
                              0 - no hit type guaranteed
                              1 - guaranteed critical hit
@@ -84,14 +85,13 @@ class Rate:
         p_c = round(self.crit_prob() + ch_mod, 10)
         p_d = round(self.direct_hit_prob() + dh_mod, 10)
 
-
         if guaranteed_hit_type == 1:
-            p_c = 1.
+            p_c = 1.0
         elif guaranteed_hit_type == 2:
-            p_d = 1.
+            p_d = 1.0
         elif guaranteed_hit_type == 3:
-            return np.array([0, 0, 0, 1.])
-        
+            return np.array([0, 0, 0, 1.0])
+
         p_cd = round(p_c * p_d, 10)
 
         return np.array(
@@ -102,3 +102,85 @@ class Rate:
                 p_cd,
             ]
         )
+
+    def get_hit_type_damage_buff(
+        self,
+        guaranteed_hit_type=0,
+        buff_crit_rate=0,
+        buff_dh_rate=0,
+        determination=None,
+    ):
+        """
+        Compute the damage buff granted to a hit-type buff acting upon an action with a guaranteed hit type.
+        guaranteed_hit_type: integer representing the guaranteed hit type:
+                             0 - normal hit (why are you calling this function)
+                             1 - critical hit
+                             2 - direct hit
+                             3 - critical direct hit
+        buff_crit_rate: how much a buff increases the crit rate by, e.g., 10% increase -> 0.1.
+                        Leave as 0 if no crit buff is present.
+        buff_dh_rate: how much a buff increases the direct hit rate by, e.g., 10% increase -> 0.1.
+                      Leave as 0 if no direct hit buff is present.
+        determination: determination stat value. Used only for direct hit buffs,
+                       as the direct hit rate stat gets added to the determination stat
+                       to create and effective determination multiplier.
+        """
+        if guaranteed_hit_type == 0:
+            return 1
+
+        # Damage buff for crit rate buff
+        unbuffed_crit_rate = self.crit_prob()
+        buffed_crit_rate = round(unbuffed_crit_rate + buff_crit_rate, 3)
+
+        crit_buff = 1 + (
+            (buffed_crit_rate - unbuffed_crit_rate) * (unbuffed_crit_rate + 0.35)
+        )
+
+        if guaranteed_hit_type == 1:
+            return crit_buff
+
+        # Damage buff for direct hit rate buff
+        unbuffed_dh_rate = self.direct_hit_prob()
+        buffed_dh_rate = round(unbuffed_dh_rate + buff_dh_rate, 3)
+
+        if determination is None:
+            raise ValueError(
+                "Determination stat value must be specified because Direct Hit rate buffs depend on the base Determination stat"
+            )
+
+        dh_buff = 1 + ((buffed_dh_rate - unbuffed_dh_rate) * 0.25)
+        # Add DH rate to determination stat for new effective det multiplier
+        dh_buff *= (
+            (
+                (
+                    (
+                        (
+                            140
+                            * (
+                                determination
+                                - self.lvl_main
+                                + self.dh_amt
+                                - self.lvl_sub
+                            )
+                        )
+                        / self.lvl_div
+                    )
+                    + 1000
+                )
+                // 1
+            )
+            / 1000,
+        )
+        # Divide out the original determination multiplier so it can be treated as a buff
+        dh_buff /= (
+            (((140 * (determination - self.lvl_main)) / self.lvl_div) + 1000) // 1
+        ) / 1000
+
+        dh_buff = round(dh_buff, 6)
+        # Works for either guaranteed direct hits or guaranteed critical-direct hits
+        # for the former, crit_buff = 1
+        return round(crit_buff * dh_buff, 6)
+
+
+if __name__ == "__main__":
+    pass
