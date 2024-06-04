db4936d34644980ae95d9fa6b307a99a3fe9b436
ffxiv-acerola <114882724+ffxiv-acerola@users.noreply.github.com>
2024-06-03 20:04:38 -0500

    Performance optimization (#18)
    * Add in performance updates:
    
    * Allow step size of final action/rotation damage distributions to be specified
    * Allow option to only compute MGF or damage distributions
    * Address edge case when one-hit pmfs are coarsened and yield all 0's
    * Add notebook with performance/error analysis
    
    * pass in compute_mgf flag for Rotation
    
    * ... also update jobs
    I need to start unit testing
    
    * fix dps compute issue

--- ffxiv_stats/__init__.py
+++ ffxiv_stats/__init__.py
@@ -6,7 +6,7 @@ This module computes exact damage DPS distributions or the first three moments (
 
 """
 
-__version__ = "0.4.5"
+__version__ = "0.4.7"
 
 from ffxiv_stats import moments, rate, jobs, modifiers
 from ffxiv_stats.moments import Rotation
