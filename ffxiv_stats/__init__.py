2b7f25dcb53664f474586cab039413719068892a
acerola <ffxivacerola@gmail.com>
2025-03-29 13:54:59 -0500

    manage package with poetry

--- ffxiv_stats/__init__.py
+++ ffxiv_stats/__init__.py
@@ -6,8 +6,6 @@ This module computes exact damage DPS distributions or the first three moments (
 
 """
 
-__version__ = "0.5.5"
-
 from ffxiv_stats import moments, rate, jobs, modifiers
 from ffxiv_stats.moments import Rotation
 from ffxiv_stats.rate import Rate
