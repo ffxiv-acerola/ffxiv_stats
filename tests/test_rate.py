4a5e29e2f46b6dee43bf87073953b1ff199acb88
acerola <ffxivacerola@gmail.com>
2025-03-30 14:45:21 -0500

    Fix how damage bonus to guaranteed hit types are computed
    Also add tests

--- tests/test_rate.py
+++ tests/test_rate.py
@@ -1,56 +1,53 @@
 from ffxiv_stats.rate import Rate
+import pytest
 
+@pytest.fixture()
+def rate():
+    return Rate(2000, 1250, 100)
 
-class TestDawntrailRates:
-    critical_hit = 2000
-    direct_hit = 1250
-    determination = 2000
-    level = 100
-    r = Rate(critical_hit, direct_hit, level)
+@pytest.fixture()
+def base_rate():
+    return Rate(420, 420, 100)
 
-    def test_critical_hit_damage_multiplier(self) -> None:
-        assert self.r.l_c == 1513
+def test_critical_hit_damage_multiplier(rate):
+    assert rate.l_c == 1513
 
-    def test_critical_hit_rate(self) -> None:
-        assert self.r.crit_prob() == 0.163
+def test_critical_hit_rate(rate) -> None:
+    assert rate.crit_prob() == 0.163
 
-    def test_direct_hit_rate(self) -> None:
-        assert self.r.direct_hit_prob() == 0.164
+def test_direct_hit_rate(rate) -> None:
+    assert rate.direct_hit_prob() == 0.164
 
-    def test_hit_type_probability(self) -> None:
-        assert (
-            (self.r.p[0] == 0.699732)
-            & (self.r.p[1] == 0.136268)
-            & (self.r.p[2] == 0.137268)
-            & (self.r.p[3] == 0.026732)
-        )
+def test_hit_type_probability(rate) -> None:
+    assert (
+        (rate.p[0] == 0.699732)
+        & (rate.p[1] == 0.136268)
+        & (rate.p[2] == 0.137268)
+        & (rate.p[3] == 0.026732)
+    )
 
-    def test_auto_crit(self) -> None:
-        """Test that a guaranteed critical hit correctly applies a critical hit rate buff as a damage buff.
-        """
-        pass
+@pytest.mark.parametrize(
+    "ch_buff, expected",
+    [(0, 1.0), (0.1, 1.051) ],
+)
+def test_guaranteed_critical_damage_multiplier(rate, ch_buff, expected):
+    multiplier = rate._guaranteed_critical_damage_multiplier(ch_buff)
+    assert multiplier == expected
 
-    def test_auto_dh(self) -> None:
-        """Test that a guaranteed direct hit has above-base direct hit stat count as a damage buff."""
-        pass
+@pytest.mark.parametrize(
+    "dh_buff, det, expected",
+    [(0, 420, 1.041), (0, 2000, 1.038), (0.2, 2000, 1.09)],
+)
+def test_guaranteed_direct_damage_multiplier(rate, dh_buff, det, expected):
+    multiplier = rate._guaranteed_direct_damage_multiplier(dh_buff, det)
+    assert multiplier == expected
 
-    def test_auto_cdh_with_crit_buff(self) -> None:
-        """Test that a guaranteed critical-direct hit correctly applies a critical hit rate buff as a damage buff.
-        """
-        pass
-
-    def test_auto_cdh_with_dh_buff(self) -> None:
-        """Test that a guaranteed critical-direct hit correctly applies a direct hit rate buff as a damage buff.
-        """
-        pass
-
-    def test_auto_cdh_with_crit_dh_buff(self) -> None:
-        """Test that a guaranteed critical-direct hit correctly applies both a critical hit rate buff and direct hit rate buff as damage buffs.
-        """
-        pass
-
-    def test_auto_dh_no_dh(self) -> None:
-        """Test a guaranteed critical-direct hit, but there is no melded direct hit stat.
-        A base direct hit rate stat should grant no damage buff.
-        """
-        pass
+@pytest.mark.parametrize(
+    "det",
+    [420, 1000],
+)
+def test_guaranteed_direct_damage_limit(base_rate, det):
+    """Test that direct hit rate buff goes to 1.0 in the limit of no direct hit rate stat.
+    """
+    multiplier = base_rate._guaranteed_direct_damage_multiplier(0, det)
+    assert multiplier == 1.0
